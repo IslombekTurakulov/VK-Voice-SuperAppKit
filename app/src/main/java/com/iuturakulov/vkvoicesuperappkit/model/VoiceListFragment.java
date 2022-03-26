03df90aab4667ac3c1b32b68372ff148319d474f
Islombek Turakulov <iuturakulov@edu.hse.ru>
2022-03-26 17:47:55 +0300

    Start

---
+++ app/src/main/java/com/iuturakulov/vkvoicesuperappkit/model/VoiceListFragment.java
@@ -0,0 +1,27 @@
+package com.iuturakulov.vkvoicesuperappkit.model;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.iuturakulov.vkvoicesuperappkit.R;
+
+public class VoiceListFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_voice_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
